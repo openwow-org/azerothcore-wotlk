273df0c0f726ae52ceed886608f7766b1841ea09
Tristan Cormier <cormiert2@outlook.com>
2024-04-10 09:23:26 -0400

    add IsResting method to the Player class

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1076,6 +1076,8 @@ public:
 
     FriendList* FriendListPtr();
 
+    bool IsResting () const { return HasPlayerFlag(PLAYER_FLAGS_RESTING); }
+
     void AddToWorld() override;
     void RemoveFromWorld() override;
 
